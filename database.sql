9269fb113d6860ae8f2c892fa69828e7a66f154b
kakeetopius <pius.kakeeto@strathmore.edu>
2025-02-15 01:13:34 +0300

    update

---
+++ database.sql
@@ -0,0 +1,35 @@
+CREATE TABLE Course (
+    courseid SERIAL NOT NULL PRIMARY KEY,
+    coursename VARCHAR(50) UNIQUE NOT NULL,
+    credit INT NOT NULL,
+    lecid INT
+);
+
+CREATE TABLE Lecturer (
+    lecid SERIAL NOT NULL PRIMARY KEY,
+    fname VARCHAR(50),
+    lname VARCHAR(50), 
+    email VARCHAR(50) UNIQUE,
+    phone VARCHAR(50) UNIQUE,
+    dob VARCHAR(50),
+    dept VARCHAR(50),
+    gender CHAR(1) CHECK(gender IN ('M', 'F'))
+);
+
+CREATE TABLE Student (
+    stdid SERIAL NOT NULL PRIMARY KEY,
+    fname VARCHAR(50),
+    lname VARCHAR(50), 
+    email VARCHAR(50) UNIQUE,
+    phone VARCHAR(50) UNIQUE,
+    dob VARCHAR(50),
+    faculty VARCHAR(50),
+    gender CHAR(1) CHECK(gender IN ('M', 'F'))
+);
+
+CREATE TABLE Enrollment (
+    enrollmentid SERIAL NOT NULL PRIMARY KEY,
+    courseid INT NOT NULL,
+    stdid INT NOT NULL, 
+    grade INT
+);
