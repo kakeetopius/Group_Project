02d11156ea9535f309001ad7a8175ede351f7b1c
kakeetopius <pius.kakeeto@strathmore.edu>
2025-02-16 14:15:52 +0300

    Cleaning main class

--- Course_Management/src/Main.java
+++ Course_Management/src/Main.java
@@ -2,19 +2,6 @@ import java.util.Scanner;
 
 public class Main{
     public static void main(String[] args) {
-        Lecturer lec1 = new Lecturer("Pius", "Kakeeto", "M", "kakeeto@gmail.com", "SCES");
-        Scanner sc = new Scanner(System.in);
-
-        StudentDAO sdao = new StudentDAO();
-        LecturerDAO ldao = new LecturerDAO();
-        EnrollmentDAO enrolldao = new EnrollmentDAO();
-        CourseDAO cdao = new CourseDAO();
-
-        Student s1 = (Student) sdao.getPersonByEmail("ejuma@gmail.com");
-        enrolldao.getCoursesForStudent(s1);
-        Course c1 = cdao.getCoursebyName("Calculus");
-        enrolldao.assignMarktoStudent(s1,c1,85);
-        s1.displayDetails();
-
+        
     }
 }
