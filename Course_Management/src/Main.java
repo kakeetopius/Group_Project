e80a201985594e0a53ed3903246fcc490729ae9b
kakeetopius <pius.kakeeto@strathmore.edu>
2025-02-18 13:23:25 +0300

    update

--- Course_Management/src/Main.java
+++ Course_Management/src/Main.java
@@ -4,20 +4,7 @@ import java.util.Scanner;
 
 public class Main{
     public static void main(String[] args) {
-        CourseDAO cdao = new CourseDAO();
-        StudentDAO sdao = new StudentDAO();
-        LecturerDAO ldao = new LecturerDAO();
-        PersonDAO pdao = new PersonDAO();
-        EnrollmentDAO edao = new EnrollmentDAO();
 
-        Course course = cdao.getCoursebyName("Database Systems");
-        edao.getMarksforCourse(course);
-        Map<String, Integer> grades = course.getGrades();
-        System.out.println("Marks for course " + course.getCoursename());
-        edao.getStudentsinCourse(course);
-        ArrayList<Student> students = course.getStudents();
-        for (Student student : students) {
-            System.out.println(student.getfname() + " " + student.getlname());
-        }
+        
     }
 }
